2d1200660fab0d77275213b54abbf64152c8e6b5
dabin9595 <besesa95@gmail.com>
2023-05-30 23:38:37 +0900

    reactRouter

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="About">
+      <h1>About</h1>
+      <p>리액트 라우터를 사용해 보는 프로젝트</p>
+    </div>
+  );
+};
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>Home</h1>
+      <p>가장 먼저 보여지는 페이지</p>
+    </div>
+  );
+};
+export default Home;
